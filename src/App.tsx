72a044470c9820ff6a7903ac7b7a930e976a8713
Gideon Aryeetey <gideonaryeetey8@gmail.com>
2022-09-25 11:17:31 +0000

    added: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,105 @@
 import React from 'react';
-import logo from './logo.svg';
+import phone from './images/dumb_phone.png';
 import './App.css';
+import useUSSD from './hooks/useUSSD';
 
 function App() {
+  const {
+    handleSubmit,
+    endpoint,
+    setEndpoint,
+    networks,
+    network,
+    setNetwork,
+    msisdn,
+    setMsisdn,
+    screen,
+    ussdinput,
+    setUssdInput,
+    handleOkClick,
+    resetSession,
+    responseType,
+  } = useUSSD();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-container">
+        <div style={{ position: 'relative' }}>
+          <div className="ussd-screen">
+            <div
+              dangerouslySetInnerHTML={{ __html: screen }}
+              className="ussd-screen-text"
+            />
+            <div>
+              <div>
+                {
+                  responseType === 'continue' && (
+                    <input
+                      className="ussd-input"
+                      onChange={e => setUssdInput(e.target.value)}
+                      value={ussdinput}
+                      type="text"
+                      placeholder="input" />
+                  )
+                }
+
+              </div>
+              <button className="ussd-btn" onClick={handleOkClick}>Ok</button>
+              {
+                responseType === 'continue' && (
+                  <button className="ussd-btn" onClick={resetSession}>Cancel</button>
+                )
+              }
+            </div>
+          </div>
+          <img src={phone} className="phone" alt="logo" />
+        </div>
+
+        <form onSubmit={handleSubmit} className="emulator-controls">
+          <label>USSD endpoint</label>
+          <div>
+            <input
+              onChange={e => setEndpoint(e.target.value)}
+              value={endpoint}
+              type="text"
+              placeholder="Endpoint"
+            />
+          </div>
+          <label>MSISDN</label>
+          <div>
+            <input
+              onChange={e => setMsisdn(e.target.value)}
+              value={msisdn}
+              type="text"
+              placeholder="MSISDN"
+            />
+          </div>
+          <label>Network/Operator</label>
+          <div>
+            <select
+              onChange={e => setNetwork(e.target.value)}
+              value={network}
+            >
+              {
+                networks.map(({ id, name }) => (
+                  <option
+                    key={id}
+                    value={id}
+                  >
+                    {name}
+                  </option>
+                ))
+              }
+            </select>
+          </div>
+          <div>
+            <button type="submit">Send</button>
+            <button type="button" onClick={resetSession}>Reset</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/hooks/useUSSD.ts
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import axios from "axios";
+
+const XMLParser = require('react-xml-parser');
+
+const networks = [
+    { id: '1', name: 'AirtelTigo' },
+    { id: '3', name: 'Glo' },
+    { id: '6', name: 'Vodafone' },
+    { id: '4', name: 'MTN' },
+];
+
+const { REACT_APP_USSD_ENDPOINT } = process.env;
+function nl2br(s: string) { return s.split(/\r?\n/).join("<br>"); }
+const useUSSD = () => {
+    const [endpoint, setEndpoint] = useState<string>(REACT_APP_USSD_ENDPOINT || '');
+    const [network, setNetwork] = useState('4');
+    const [sessionid, setSessionId] = useState((new Date()).getTime());
+    const [msisdn, setMsisdn] = useState<string>('');
+    const [ussdinput, setUssdInput] = useState<string>('1');
+    const [screen, setScreen] = useState<string>('');
+    const [responseType, setResponseType] = useState<string>('');
+
+    const handleSubmit = (e: any) => {
+        if (e) e.preventDefault();
+        const body = {
+            pryussd_req: {
+                msisdn,
+                ussdinput,
+                network,
+                sessionid: sessionid.toString(),
+            }
+        }
+        axios.post(endpoint, body)
+            .then(resp => {
+                const { data } = resp;
+                const dataCleaned = nl2br(data);
+                const regex = /<ussdmsg>(.*?)<\/ussdmsg>/g;
+                const replaceRegex = /<ussdmsg>|<\/ussdmsg>/g
+                const found = dataCleaned.match(regex);
+                const ussdmsg = found ? found[0].replaceAll(replaceRegex, '') : '';
+                const xml = new XMLParser().parseFromString(data);
+                const resptype = xml?.children?.find(({ name }: { name: string }) => name === 'resptype')?.value;
+                setScreen(ussdmsg);
+                setResponseType(resptype);
+            })
+            .catch(e => {
+                alert(e.message)
+            });
+    };
+
+    const handleOkClick = () => {
+        if (responseType.toLowerCase() !== 'continue') {
+            resetSession();
+            setUssdInput('1');
+        }
+        handleSubmit(null);
+        return;
+    }
+
+    const resetSession = () => {
+        setSessionId((new Date()).getTime());
+    }
+
+    return {
+        handleSubmit,
+        endpoint,
+        setEndpoint,
+        networks,
+        network,
+        setNetwork,
+        msisdn,
+        setMsisdn,
+        screen,
+        ussdinput,
+        setUssdInput,
+        handleOkClick,
+        resetSession,
+        responseType,
+    }
+}
+
+export default useUSSD;
